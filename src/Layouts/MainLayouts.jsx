d9eb68a1f753400f664e7e5beb32b043b6cb4008
Israt85 <isratanjuman3@gmail.com>
2024-11-05 21:51:24 +0600

    added footer

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
+import Footer from "../Shared/Footer/Footer";
 
 const MainLayouts = () => {
     return (
         <div className="max-w-7xl mx-auto min-h-screen bg-base-100">
            <Navbar></Navbar>
            <Outlet></Outlet> 
+           <Footer></Footer>
         </div>
     );
 };
