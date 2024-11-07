0a23389cb76a1103a54876da3ff9feb69566e6d3
Israt85 <isratanjuman3@gmail.com>
2024-11-07 21:02:21 +0600

    completed blog page

--- src/Pages/Blog/Blog.jsx
+++ src/Pages/Blog/Blog.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BlogBanner from './BlogBanner';
 import AllBlogs from './AllBlogs';
+import Trending from './Trending';
 
 const Blog = () => {
     return (
         <div className='bg-base-100'>
             <BlogBanner></BlogBanner>
             <AllBlogs></AllBlogs>
+            <Trending></Trending>
         </div>
     );
 };
---
+++ src/Pages/Blog/Trending.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+const Trending = () => {
+    return (
+        <div className='w-full h-[800px]'>
+            <div className='w-full h-[630px] bg-gradient-to-t from-[#800080] to-[#000000]'>
+                <div className='text-center pt-10 text-white'>
+                    <p className='text-7xl font-bold font-teko'>Trending</p>
+                    <p className='text-lg mx-auto w-[550px]'>Stay updated with the latest fitness trends, tips, and insights to elevate your health journey!</p>
+                </div>
+              <div className='flex mt-10 justify-center items-center'>
+              <div className='w-[350px] mx-auto my-6 gap-6 shadow-2xl bg-white h-[480px]'>
+                             <img className='p-4' src="https://s3-alpha-sig.figma.com/img/9ca4/12fa/840de131974162b09d68033e23a850b5?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Jo4XJR7WUiUvDB7yrqc8p4kjGtwM8RrC7HbVimrK2~0O2VLhM6QrtsgbU6Drq5F5j4jaArVX6CIJAS-X-2em-FjmgA5PzsBmQcnUXQ7B7nvUfHvNzt5DaeDVYIw3uEptv3SWA8ox63lKlzJIFROCGYpGnpx6k7gddEzTkTiWQD7lObOq8cOLjbxGoOHF~Fx0wBZSmjkhF52sjlIpdqnAgl8SqfqroVOU7vOckIVWiaoQaYI0burTGeepW4CnBgFs6eZKw7B3TJ8DyA9Bkh4f2pdkeqw65RNUnFQ0xmfNeXphgdNS0gIPqBrx07XrNltwF5Gdbo-RL7w-IWczdvpATQ__" alt="" />
+                            <div className='py-2 flex justify-between items-center px-4'>
+                            <p className='text-base font-bold'>The Rise of Hybrid Workouts</p>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7 17L17 7M17 7H7M17 7V17" stroke="#101828" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                            </div>
+                             <p className='px-4 text-sm py-2'>Explore how hybrid workouts are gaining popularity and how to incorporate them into your routine for maximum benefits.</p>
+
+                             <div className='flex items-center py-4 gap-2'>
+                                <div>
+                                    <img className='w-16 h-16 rounded-full' src="https://s3-alpha-sig.figma.com/img/ca26/9fff/9961afb9c6a84bffddcb988a6fad7166?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UB7VbkVvk~WhP4pe7HyZKaA9-bvKCEOM7Ldouib9jaWqtB35XXPVScHg5JmDb1YCT4aKezkcBT1NSFYxMMO4QGI~fGVhvQisptWVQH-tvfhyXNkzzMrzsK1DiLIso6FUh1biPRxT2OWBmtBqBrad8aBXdnGF2HCJ0Dv5yMZH6sqHayZMMD6rDu5rNRdcGqiHHwNG97MntY67ayjNWy3DX1XZ4NpdU1VAayU4B2uaiOf4w3ulLiRpAcseB5t485aH7c7ogN3Ov3jPEQWXxqMFIJMcXS0N-PgqeGbHYAnZe6Ro7aDrTJIorbcGHq~6QojswzuYHR33qIgQGDzK2Fb0rg__" alt="" />
+                                </div>
+                                <div>
+                                    <p className='font-bold'>Natali Craig</p>
+                                    <p>20 Oct 2024</p>
+                                </div>
+                             </div>
+                    </div>
+              <div className='w-[350px] mx-auto my-6 gap-6 shadow-2xl bg-white h-[480px]'>
+                             <img className='p-4' src="https://s3-alpha-sig.figma.com/img/0095/33dd/b18880647940253fa905f2a8d6a3a95e?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hyO9dvh6upc47iMqEWPqIdCy25sevlnCn0VnocY43jgb2l9dV8ca3ISy0QU90JTdkocUoDSxlSDouDVZb1-MNbptdwzp-cqCLNtv0pUBqRJ-~I41uAL-Mu-Q7BXQ2xvibX7DTdf0FGqE5NuOpNKpPvr0WAnU7yfYk7hbnMKz6K7vQ5L0F3jlQ-uyzJipRlthgvJvahCBTjJOzDBxRHK3OKISVDYh3rpGhLrKLWG0YGcKqy5jN-WsWVAI29EfQYiLv9VTLbyyRC7f~5ZauL2k6my~3v~DilWfMuvxTj-nLy3l4LPuWVTTO56W-AKTrZXV91-yMxRhXT57nhiCEy6L1g__" alt="" />
+                            <div className='py-2 flex justify-between items-center px-4'>
+                            <p className='text-base font-bold'>Mindfulness in Fitness</p>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7 17L17 7M17 7H7M17 7V17" stroke="#101828" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                            </div>
+                             <p className='px-4 text-sm py-2'>Learn about the growing trend of integrating mindfulness practices into fitness routines to enhance both mental and physical health.</p>
+
+                             <div className='flex items-center py-4 gap-2'>
+                                <div>
+                                    <img className='w-16 h-16 rounded-full' src="https://s3-alpha-sig.figma.com/img/2e2c/f1b6/f441c6f28c3b0e1e0eb4863eb80b7401?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a~fUXA1e46IxyLgzgvBJekR~8gwcz~n73yWP7iQ2xaI6BIqGPF8e-N6VySdOLZGYztqPaRU~QECzlxaqZb9uLUNnAMbVNep7ldmeVsSoFvCriXopwV~P0zHj4WrpsRZ~ZYMcZv269aCuWk34BqNLoXaf78j20EqZOvjvQqyzDdbDj2pWvGwQDT7VzXxFL5jVULJkzmZI~9mXYUrS-D9yl6~1b-V~MMaCQnB64i8~z4fOHLg3WTfcFfv84Mt8Id08sOEpz4cE-3h157O4ZOUoSI-1PgZMeYR94G7bGwQ89tWHbO1Cfa0nVIgFeFSYKmtYmkaajn7bwGmlqTVqHSnIVg__" alt="" />
+                                </div>
+                                <div>
+                                    <p className='font-bold'>Drew Cano</p>
+                                    <p>13 Oct 2024</p>
+                                </div>
+                             </div>
+                    </div>
+              <div className='w-[350px] mx-auto my-6 gap-6 shadow-2xl bg-white h-[480px]'>
+                             <img className='p-4' src="https://s3-alpha-sig.figma.com/img/c220/b565/fba3f3369a85cb9bd4d04bf17c85be21?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=VXMJzbZw4eEFPIo8whOJwb56s4V77ielMZzpvUP5Gk0iViGHZoAB9CD5qBEM3-fMRr6b5MOiRQ7A~boS8n1TPxsisQiWsLTjAaBjpAPXwStLm4pjsrhzXvzcnCKmkU4Y9rH0f6GbbuYiCPoSIkkUP5CImMoMWX4qgA9tatEQNBtXDxpg~Y5QDiT42XEgZvbJwAr5slICvnR3ljJua4hTjg69PXI7IIbu5dsaSQMLXaclnAxmyFFsyVYArEyPQOtoq8DXLxGAkUmPapPGukbDVgon9s3jTk0vQO6kGeHb9YwL8Lr-7miQnscgZ88rQXyIqmv-Z8guXaLzgLGvQ8paxg__" alt="" />
+                            <div className='py-2 flex justify-between items-center px-4'>
+                            <p className='text-base font-bold'>Nutrition Hacks</p>
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7 17L17 7M17 7H7M17 7V17" stroke="#101828" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                            </div>
+                             <p className='px-4 text-sm py-2'>Discover practical meal prep tips inspired by fitness influencers to simplify your nutrition tracking and stay on track.</p>
+
+                             <div className='flex items-center py-4 gap-2'>
+                                <div>
+                                    <img className='w-16 h-16 rounded-full' src="https://s3-alpha-sig.figma.com/img/555c/b373/5701db8d4318f0d93edd1f4b64493b37?Expires=1731888000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=I7slTrR7l57WftbyRVR4t2VlAUl5B42xFN2U2y8J7sDIv67uTlOwQ-P0RVYb11UI4SFgM7cLmk2XjB~QTthWofZATD0ZB40DqNiL8Xc8mO7nwy886B5m6zVijTJqPr8ECbAZsmbDU9rnbe-7maEw5cRQtjQI-hDbBPmo9j~1JRZdpYl44UNitISL6QwWz1m86LdayFqiNoCwfUKAx3yk61tnWz6aQUYKxtZZNixT-5RA6g-WYs-iM8d-5GGKJ5ZpS623eOffjBKxNy0Q8Uvl5ywCGtTM-7J2QmusDlYBEEZw85ZRYSdoytPlrjZuAtGw-MbD950gGuIJ7kPMUCryKQ__" alt="" />
+                                </div>
+                                <div>
+                                    <p className='font-bold'>Orlando Diggs</p>
+                                    <p>10 Jan 2024</p>
+                                </div>
+                             </div>
+                    </div>
+              </div>
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default Trending;
